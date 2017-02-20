b4ebeb164f9940d9787bb7494bf3c17f69190762
Tu Do <tuhdo1710@gmail.com>
2017-02-20 17:28:03 +0700

    Update publish script

--- book_src/publish.sh
+++ book_src/publish.sh
@@ -2,15 +2,16 @@
 
 # create build dir
 for d in $(find images/ -type d); do
-    mkdir -p  build/$d
+    mkdir -p build/$d
 done
 
 # export .lyx figures to .tex
-for f in $(find images/ -name "*.lyx"  -type f); do
+cd build/
+for f in $(find ../images/ -name "*.lyx"  -type f); do
     echo $(echo $f | sed  -e "s/images\//build\/images\//g"  -e "s/.lyx/.tex/g")
     lyx -E latex $(echo $f | sed  -e "s/images\//build\/images\//g"  -e "s/.lyx/.tex/g") $f
 done
 
-# # process the images
-find build/images/ -name '*.tex' -type f -exec sed -i -e '/\\begin{document}/,$!d' \
+# process the images
+find ../build/images/ -name '*.tex' -type f -exec sed -i -e '/\\begin{document}/,$!d' \
      -e '/\\begin{document}/d' -e '/\\end{document}/d'  -- {} +
